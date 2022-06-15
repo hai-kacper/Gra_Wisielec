51e6c25e8ab9fe360024876480b94e459f5f803a
Oliwia Paliwoda <97446500+oliwia-paliwoda@users.noreply.github.com>
2022-06-15 18:27:11 +0200

    Delete wisielec directory

--- wisielec/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "wszystko.h"
-
-int main()
-{
-    cout << "Gra w wisielca. Nacisnij ENTER aby rozpoczac" << endl;
-    cin.ignore();
-    menu();
-    return 0;
-}
--- wisielec/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdbool.h>
-#include <string>
-
-using namespace std;
-
-int main() {
-return false;
-}
